c53be3215156090493d5a7030120808ba670bf6a
Yogeshwaran S <75844322+Yogeshwaran09102019@users.noreply.github.com>
2022-01-16 19:23:06 +0530

    Add files via upload

---
+++ Emojipedia/src/emojipedia.js
@@ -0,0 +1,214 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "😀",
+    name: "Grinning Face",
+    meaning:
+      "The happy smiling face is one of the most common emojis and universally applicable: you just want to say hello, express joy or excitement about something or brighten up a short text."
+  },
+  {
+    id: 5,
+    emoji: "😁",
+    name: "Grinning Face With Smiling Eyes",
+    meaning:
+      "Happy face with a mischievous laugh. Represents lightheartedness and exuberance. Full of joy due to an event, excited and agitated or just a bit embarrassed."
+  },
+  {
+    id: 6,
+    emoji: "😂",
+    name: "Face With Tears of Joy",
+    meaning:
+      "I am laughing to hard that I can hardly keep myself on my chair! Something is so incredibly funny that you cry laughing. Gets the giggles and can hardly contain himself. This emoji is the most popular and was named Word of the Year 2015."
+  },
+  {
+    id: 7,
+    emoji: "😊",
+    name: "Smiling Face With Smiling Eyes",
+    meaning:
+      "Is grinning shyly and embarrassed. The red cheeks are an expression of joy. Represents happiness, contentment, peace of mind and gratitude. Text-based variant: ^^."
+  },
+  {
+    id: 8,
+    emoji: "🙂",
+    name: "Slightly Smiling Face",
+    meaning:
+      "A light smile means you are satisfied with yourself and the world. Can make a statement sound friendlier than it might have been meant. Or have a sarcastic meaning when a smile does not actually fit the content."
+  },
+  {
+    id: 9,
+    emoji: "🙃",
+    name: "Upside Down Face",
+    meaning:
+      "Don't take me seriously! The message is either ambiguous, ironic or joking. Person plays the fool and horses around, or in response to a joke."
+  },
+  {
+    id: 10,
+    emoji: "😉",
+    name: "Winking Face",
+    meaning:
+      "Smiley blinks mischievously with one eye. Expression of humor, has been pulling a practical joke, is flirting with you or has an ulterior motive. The wink weakens the message: do not take it seriously, regard it as humorous."
+  },
+  {
+    id: 11,
+    emoji: "😍",
+    name: "Smiling Face With Heart-Eyes",
+    meaning:
+      "Happily beaming face with heart-shaped eyes. Insanely in love: with a person, a place or an object. Expresses the extent of love and affection as well as gratitude and is often part of romantic messages. Just does not know what to do with all the luck."
+  },
+  {
+    id: 12,
+    emoji: "🥰",
+    name: "Smiling Face with three hearts",
+    meaning:
+      "You are in love, sitting on cloud number nine. The romantic emoji with the enamored expression of the face stands for affairs of the heart."
+  },
+  {
+    id: 13,
+    emoji: "😋",
+    name: "Face Savouring Delicious Food",
+    meaning:
+      "Bon appetit! Smiley is licking the corner of its mouth with its tongue. Is hungry, just cooked something very delicious or eaten a whole dish. Often interpreted as a grimace, with tongue outstretched, and used after a funny message or when fooling around."
+  },
+  {
+    id: 14,
+    emoji: "🤨",
+    name: "Face With a Raised Eyebrow",
+    meaning:
+      "With the questioning look on the face, resembles the actor “The Rock“ (Dwayne Johnson). Can be used to express skepticism, disbelief or disapproval."
+  },
+  {
+    id: 15,
+    emoji: "🧐",
+    name: "Face With Monocle",
+    meaning:
+      "A monocle is a visual aid with only one glass. At the end of the 19th century it was a status symbol. Something seems suspicious! The smiley makes a warning look around. The “upper-class“ version of the pondering smiley."
+  },
+  {
+    id: 16,
+    emoji: "🤓",
+    name: "Nerd Face",
+    meaning:
+      "Huge glasses, awkward smile and buck teeth. Used humorously or ironically for nerds or to express how smart you are. Stereotype of a nerd; a smart but funny-dressed person with social deficits."
+  },
+  {
+    id: 17,
+    emoji: "😎",
+    name: "Face with Sunglass",
+    meaning:
+      "I'm the king of the world! Mr. Cool Smiley with sunglasses. Is totally self-assured and relaxed. Everything is absolutely easy and cool. Also a symbol for sun, summer holidays, and holidays on the beach."
+  },
+  {
+    id: 18,
+    emoji: "🤩",
+    name: "Face with Starry Eyes",
+    meaning:
+      "Is very excited about something and already full of anticipation. Overwhelmed and speechless after meeting someone you like a lot. Fascinated by the glory and the world of the stars."
+  },
+  {
+    id: 19,
+    emoji: "🥳",
+    name: "Face with party blower and party hat",
+    meaning:
+      "That was a wild party or “Let's celebrate“. No matter what joyful event is in view, it will be celebrated appropriately. May also stand for the carnival season."
+  },
+  {
+    id: 20,
+    emoji: "😏",
+    name: "Smirking/Whimsical Face",
+    meaning:
+      "Half a smile that oozes with complacency. Represents irony, coolness or playfulness. Be careful: Somebody could be up to something. Used as an offensive look for flirting or sexual innuendo."
+  },
+  {
+    id: 21,
+    emoji: "😒",
+    name: "Unamused Face",
+    meaning:
+      "The grumpy, sullen gaze expresses dissatisfaction. Is not enthusiastic about a thing and shows that. Expression of disinterest and disapproval."
+  },
+  {
+    id: 22,
+    emoji: "😞",
+    name: "Disappointed Face",
+    meaning:
+      "Face is drooping and directed downwards. Smiley is very sad and frustrated and gives a disappointed impression. Represents sorrow, remorse and regret."
+  },
+  {
+    id: 23,
+    emoji: "😕",
+    name: "Confused Face",
+    meaning:
+      "Is confused or does not agree with something. Is uncertain or disappointed, had imagined this in a different way."
+  },
+  {
+    id: 24,
+    emoji: "😩",
+    name: "Weary Face",
+    meaning:
+      "Leave me alone! Reluctant smiley with raised eyebrows and mouth downturned, moaning about grueling, unpleasant but inevitable things. Is weepy, upset and completely exhausted, mentally or physically."
+  },
+  {
+    id: 25,
+    emoji: "🥺",
+    name: "Face With Begging Eyes",
+    meaning:
+      "The big eyes are shining, devoted facial expression. The emoji is entreatingly begging for something: wants to borrow some money or the car. If you cannot get ahead by asking for something, puppy dog eyes are the last resort to get what you want."
+  },
+  {
+    id: 26,
+    emoji: "😭",
+    name: "Loudly Crying Face",
+    meaning:
+      "Emoji is both very sad and distraught, or is dying of laughter. Tears are pouring out of the eyes like a waterfall. Represents injury, pain or defeat. Is also used ironically or something is so funny that you die of laughter."
+  },
+  {
+    id: 27,
+    emoji: "😡",
+    name: "Pouting Face",
+    meaning:
+      "The pouting face expresses displeasure with a person or situation. Beware, danger of explosion! Furious emoji face. Is annoyed and already sulking with a deep-red face."
+  },
+  {
+    id: 28,
+    emoji: "😳",
+    name: "Flushed Face",
+    meaning:
+      "Person is shocked, in an uncomfortable situation or has made a mistake. Expresses shame; something is very embarrassing. Reacts to a compliment or a flattering message that the person did not want to receive."
+  },
+  {
+    id: 29,
+    emoji: "🥵",
+    name: "Overheated Face",
+    meaning:
+      "It is so hot, I will get a heat stroke right away! Bright red head, the tongue is hanging from the mouth and drops of sweat are on the forehead. The hottest place in the world lies in Iran: In 2005, the highest temperature of 70.7 °C was measured in the Dasht-e Lut desert."
+  },
+  {
+    id: 30,
+    emoji: "🤗",
+    name: "Hugging Face",
+    meaning:
+      "Smiley with red cheeks is stretching out its hands toward you. Wants to hug you. It's an open and cordial gesture and an expression of warmth and friendliness."
+  }
+];
+
+export default emojipedia;
